bdab295d146a39dbd1522772cb996ab6dc49ce7d
unknown <shiv.jangir@dotsquares.com>
2026-07-07 12:01:53 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,25 @@
+# ------------------------------
+# Operators
+# ------------------------------
+
+print("\nOperators")
+
+a = 20
+b = 6
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+# O/p
+# Addition: 26
+# Subtraction: 14   
+# Multiplication: 120
+# Division: 3.3333333333333335
+# Floor Division: 3
+# Modulus: 2
+# Power: 64000000
