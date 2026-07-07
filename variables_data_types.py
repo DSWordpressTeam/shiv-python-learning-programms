a1c3d9a1de46ac174a57ae4a17c6f61a668df3d8
unknown <shiv.jangir@dotsquares.com>
2026-07-07 11:37:06 +0530

    All Python Programmes

---
+++ variables_data_types.py
@@ -0,0 +1,20 @@
+# ------------------------------
+# Variables & Data Types
+# ------------------------------
+
+name = "Shiv"
+age = 22
+salary = 50000.75
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Salary:", salary)
+print("Student:", is_student)
+
+
+# O/p:- 
+# Name: Shiv
+# Age: 22
+# Salary: 50000.75
+# Student: True
